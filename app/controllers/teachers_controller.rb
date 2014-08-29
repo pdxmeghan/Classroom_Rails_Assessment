534943887e0071c3e58ddc36cbca6d3a2c1af9c6
Meghan Lindsley <pdxmeghan@gmail.com>
2014-08-28 20:43:07 -0700

    create routes, controller methods, and views ffor new, create, and show

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -3,4 +3,24 @@ class TeachersController < ApplicationController
     @teachers = Teacher.all
     render('teachers/index.html.erb')
   end
+
+  def new
+    @teacher = Teacher.new
+    render('teachers/new.html.erb')
+  end
+
+  def create
+    @teacher = Teacher.create(params[:teacher])
+    if @teacher.save
+      flash[:notice] = "Your teacher has been added"
+      redirect_to("/teachers/#{@teacher.id}")
+    else
+      render('teachers/new.html.erb')
+    end
+  end
+
+  def show
+    @teacher = Teacher.find(params[:id])
+    render('teachers/show.html.erb')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   match('/', {:via => :get, :to => 'teachers#index'})
-  match('/teachers', :via => :get, :to => 'teachers#index')
+  match('/teachers', {:via => :get, :to => 'teachers#index'})
+  match('/teachers/new', {:via => :get, :to => 'teachers#new'})
+  match('/teachers/:id', {:via => :get, :to => 'teachers#show'})
+  match('/teachers', {:via => :post, :to => 'teachers#create'})
 end
